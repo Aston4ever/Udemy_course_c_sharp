eb68b98b0791c6d2b6621b383cb38a2aa8c9cfaf
aston <aston4ever@gmail.com>
2025-05-11 10:44:29 +0300

    learning properties

---
+++ Classes/Car.cs
@@ -0,0 +1,30 @@
+﻿namespace Classes;
+
+public class Car
+{
+    public string Model { get; set; }
+    public string Brand
+    {
+        get => Model;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                Brand = "Default";
+            }
+            else
+            {
+                Brand = value;
+            }
+        }
+    }
+    
+    public Car(string model, string brand)
+    {
+        Model = model;
+        Brand = brand;
+        
+    }
+    
+    
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Classes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var audi = new Car("Audi", "q7");
+        Console.WriteLine(audi.Model);
+    }
+}
